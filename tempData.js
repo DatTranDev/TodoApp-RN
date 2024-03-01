385351ea0580b380ef0f0fafbb189b6b736b4e5d
Tran Thanh Dat <neban0444@gmail.com>
2024-03-01 16:48:16 +0700

    first cm

---
+++ tempData.js
@@ -0,0 +1,73 @@
+// Temporay Data until we connect with Firebase
+
+export default tempData = [
+    {
+        id: 1,
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Going Intern",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Falling in Love",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    }
+]
